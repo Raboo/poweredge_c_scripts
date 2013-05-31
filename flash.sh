affb7f53f42e89da06e4e340a24a55d940f462d7
Elias Abacioglu <elias.rabi@gmail.com>
2013-05-31 15:59:06 +0200

    fixes to flash.sh

--- flash.sh
+++ flash.sh
@@ -13,19 +13,18 @@ BMC_IMG=BMC_2013-05-31.fw
 FCB_IMG=FCB_2013-05-31.fw
 BIOS_IMG=BIOS_2013-05-31.fw
 
-IPMIFLASH="echo ./ipmiflash"
-FLAGS="-n"
-#TFTPSVR=192.168.8.174
-HTTPSVR="http://10.24.2.100/poweredge_c_fw"
+IPMIFLASH="echo /opt/dell/pec/ipmiflash"
+FLAGS="-p"
+FWREPO="http://10.24.2.100/poweredge_c_fw"
 BMCUSER="root"
 BMCPASS="root"
 
 for server in ${TGT_SVR_BMC}; do
 	# BMC
-	${IPMIFLASH} ${FLAGS} -H${server} -U${BMCUSER} -P${BMCPASS} bmc http://${HTTPSVR}/${BMC_IMG}
+	${IPMIFLASH} ${FLAGS} -H${server} -U${BMCUSER} -P${BMCPASS} bmc ${FWREPO}/${BMC_IMG}
 	# FCB
-	${IPMIFLASH} ${FLAGS} -H${server} -U${BMCUSER} -P${BMCPASS} fcb http://${HTTPSVR}/${FCB_IMG}
+	${IPMIFLASH} ${FLAGS} -H${server} -U${BMCUSER} -P${BMCPASS} fcb ${FWREPO}/${FCB_IMG}
 	# BIOS (will cause hard reboot of machine)
-	${IPMIFLASH} ${FLAGS} -H${server} -U${BMCUSER} -P${BMCPASS} bios http://${HTTPSVR}/${BIOS_IMG}
+	${IPMIFLASH} ${FLAGS} -H${server} -U${BMCUSER} -P${BMCPASS} bios ${FWREPO}/${BIOS_IMG}
 done
 
